5c01a830fac4cd55a26e2bb5495717d34ce69752
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2020-10-19 21:13:05 +0200

    Add basic code structure

--- console-screen-background/Program.cs
+++ console-screen-background/Program.cs
@@ -6,7 +6,16 @@ namespace console_screen_background
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int consoleHeight = Console.WindowHeight,
+                consoleWidth = Console.WindowWidth;
+            Random rdnNb = new Random();
+
+            Console.SetCursorPosition(consoleWidth-10, consoleHeight-10);
+            Console.WriteLine($"Height: {consoleHeight} | Width: {consoleWidth}");
+
+            Console.SetCursorPosition(rdnNb.Next(0, consoleHeight-1), rdnNb.Next(0, consoleWidth));
+            Console.ForegroundColor = (ConsoleColor) rdnNb.Next(0,15);
+            Console.Write('█');
         }
     }
 }
